0e64eaee4450117c4990cb2ae6d0636361003003
Shashank Sharma <shashank.sharma@seekadegree.com>
2021-01-15 12:18:41 +0530

    @Shashank add blog model class

---
+++ src/main/java/com/cozentus/sbms/domain/Blog.java
@@ -0,0 +1,59 @@
+package com.cozentus.sbms.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "blog")
+public class Blog {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "title", nullable = false, length = 50)
+	private String title;
+	
+	@Column(name = "synopsis", length = 100)
+	private String synopsis;
+	
+	@Column(name = "author_id")
+	private Long authorId;
+	
+	@Column(name = "co_author_id")
+	private Long coAuthorId;
+	
+	@Column(name = "blog_link")
+	private String blogLink;
+	
+	@Column(name = "status")
+	private String status;
+	
+	@OneToOne
+	@JoinColumn(name = "topic_id", referencedColumnName = "id")
+	private Topic topic;
+	
+	@Column(name = "created_date", nullable = false)
+	private Date createdDate;
+
+	@Column(name = "created_by", length = 50)
+	private String createdBy;
+
+	@Column(name = "updated_date", nullable = false)
+	private Date updatedDate;
+
+	@Column(name = "updated_by", length = 50)
+	private String updatedBy;
+}
