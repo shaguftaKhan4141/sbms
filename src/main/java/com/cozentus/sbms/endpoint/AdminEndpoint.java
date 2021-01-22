d09653e1daf9b595df766b65c9568ec3568615df
Shashank Sharma <shashank.sharma@seekadegree.com>
2021-01-22 15:02:48 +0530

    @Shashank add authorization in Swagger

--- src/main/java/com/cozentus/sbms/endpoint/AdminEndpoint.java
+++ src/main/java/com/cozentus/sbms/endpoint/AdminEndpoint.java
@@ -4,11 +4,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cozentus.sbms.error.InvalidDataException;
 import com.cozentus.sbms.error.NotFoundException;
 
+@RequestMapping("/admin")
 public interface AdminEndpoint {
 
 	@GetMapping(value = "/user/role/{roleId}")
